4edf72a53fc524ab15a803f0f7bb3b996945e080
BuildTools <unconfigured@null.spigotmc.org>
2022-09-03 14:48:33 +0300

    improved menu GUI

--- src/main/java/Listeners/MenuListener.java
+++ src/main/java/Listeners/MenuListener.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -15,13 +16,13 @@ import java.util.UUID;
 
 public class MenuListener implements Listener {
 
-        private HashMap<UUID, Long> cooldown;
+    private HashMap<UUID, Long> cooldown;
 
-        public MenuListener(){
+    public MenuListener() {
 
-            this.cooldown = new HashMap<>();
+        this.cooldown = new HashMap<>();
 
-        }
+    }
 
     @EventHandler
     public void KitMenu(InventoryClickEvent event) {
@@ -32,14 +33,14 @@ public class MenuListener implements Listener {
 
             HumanEntity humanEntity = event.getWhoClicked();
 
-            if(event.getCurrentItem() == null){
+            if (event.getCurrentItem() == null) {
                 return;
             }
 
             // first time writing that command
-            if(!this.cooldown.containsKey(humanEntity.getUniqueId())){
+            if (!this.cooldown.containsKey(humanEntity.getUniqueId())) {
 
-                this.cooldown.put(humanEntity.getUniqueId(),System.currentTimeMillis());
+                this.cooldown.put(humanEntity.getUniqueId(), System.currentTimeMillis());
 
 
                 if (event.getCurrentItem().getType() == Material.COAL) {
@@ -47,15 +48,15 @@ public class MenuListener implements Listener {
                     //Starter items
 
 
-                    ItemStack helmet = new ItemStack(Material.LEATHER_HELMET,1);
+                    ItemStack helmet = new ItemStack(Material.LEATHER_HELMET, 1);
                     ItemMeta editHelmet = helmet.getItemMeta();
                     editHelmet.setDisplayName(ChatColor.LIGHT_PURPLE + "Bere");
-                    editHelmet.addEnchant(Enchantment.DURABILITY,10,true);
-                    editHelmet.addEnchant(Enchantment.OXYGEN, 5,true);
-                    editHelmet.addEnchant(Enchantment.MENDING, 1,true);
+                    editHelmet.addEnchant(Enchantment.DURABILITY, 10, true);
+                    editHelmet.addEnchant(Enchantment.OXYGEN, 5, true);
+                    editHelmet.addEnchant(Enchantment.MENDING, 1, true);
                     helmet.setItemMeta(editHelmet);
 
-                    humanEntity.getInventory().setItem(23,helmet);
+                    humanEntity.getInventory().setItem(23, helmet);
                     humanEntity.getInventory().setItem(24, new ItemStack(Material.COOKED_MUTTON, 32));
                     humanEntity.getInventory().setItem(25, new ItemStack(Material.OAK_LOG, 32));
                     humanEntity.getInventory().setItem(26, new ItemStack(Material.TORCH, 32));
@@ -66,14 +67,12 @@ public class MenuListener implements Listener {
                     humanEntity.sendMessage(ChatColor.RED + "Bu ekipmanı alacak yetkiye sahip değilsiniz.");
                 }
 
-            }
-            else{
+            } else {
                 // Already typed that command
                 //difference in ms
 
                 long timeElapsed = System.currentTimeMillis() - cooldown.get(humanEntity.getUniqueId());
-                if(timeElapsed >= 1000){
-
+                if (timeElapsed >= 1000) {
 
 
                     if (event.getCurrentItem().getType() == Material.COAL) {
@@ -81,15 +80,15 @@ public class MenuListener implements Listener {
                         //Starter items
 
 
-                        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET,1);
+                        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET, 1);
                         ItemMeta editHelmet = helmet.getItemMeta();
                         editHelmet.setDisplayName(ChatColor.LIGHT_PURPLE + "Bere");
-                        editHelmet.addEnchant(Enchantment.DURABILITY,10,true);
-                        editHelmet.addEnchant(Enchantment.OXYGEN, 5,true);
-                        editHelmet.addEnchant(Enchantment.MENDING, 1,true);
+                        editHelmet.addEnchant(Enchantment.DURABILITY, 10, true);
+                        editHelmet.addEnchant(Enchantment.OXYGEN, 5, true);
+                        editHelmet.addEnchant(Enchantment.MENDING, 1, true);
                         helmet.setItemMeta(editHelmet);
 
-                        humanEntity.getInventory().setItem(23,helmet);
+                        humanEntity.getInventory().setItem(23, helmet);
                         humanEntity.getInventory().setItem(24, new ItemStack(Material.COOKED_MUTTON, 32));
                         humanEntity.getInventory().setItem(25, new ItemStack(Material.OAK_LOG, 32));
                         humanEntity.getInventory().setItem(26, new ItemStack(Material.TORCH, 32));
@@ -97,23 +96,58 @@ public class MenuListener implements Listener {
                         humanEntity.closeInventory();
                         humanEntity.sendMessage(ChatColor.AQUA + "Eşyalar temin edildi!");
 
-                        this.cooldown.put(humanEntity.getUniqueId(),System.currentTimeMillis());
+                        this.cooldown.put(humanEntity.getUniqueId(), System.currentTimeMillis());
+
+                    } else if (event.getCurrentItem().getType() == Material.IRON_INGOT) {
+
+
+                        humanEntity.sendMessage(ChatColor.AQUA + "Eşyalar temin edildi!");
+
+                    } else if (event.getCurrentItem().getType() == Material.GOLD_INGOT) {
+
+                        humanEntity.sendMessage(ChatColor.AQUA + "Eşyalar temin edildi!");
+
+                    } else if (event.getCurrentItem().getType() == Material.DIAMOND) {
+
+                        humanEntity.sendMessage(ChatColor.AQUA + "Eşyalar temin edildi!");
+
+                    } else if (event.getCurrentItem().getType() == Material.EMERALD) {
+
+                        humanEntity.sendMessage(ChatColor.AQUA + "Eşyalar temin edildi!");
 
                     } else {
                         humanEntity.sendMessage(ChatColor.RED + "Bu ekipmanı alacak yetkiye sahip değilsiniz.");
                     }
-                }
-                else{                                                            // a whole day = 86.400.000 in minecraft
-                    humanEntity.sendMessage(ChatColor.AQUA + "Bekleme süresinde: " + ((10000 -  timeElapsed) / 1000 + " saat kaldı" ));
+                } else {                                                            // a whole day = 86.400.000 in minecraft
+                    humanEntity.sendMessage(ChatColor.AQUA + "Bekleme süresinde: " + ((10000 - timeElapsed) / 1000 + " saat kaldı"));
 
                 }
 
             }
 
 
+        }
+        else if(event.getView().getTitle().equals(ChatColor.LIGHT_PURPLE + "Yardım Menüsü")){
+
+            event.setCancelled(true);
+            LivingEntity livingEntity = event.getWhoClicked();
+
+            if(event.getCurrentItem() == null){
+                return;
+            }
+
+            if(event.getCurrentItem().getType() == Material.TNT){
+
+                livingEntity.setHealth(0.00);
+                livingEntity.sendMessage(ChatColor.RED + "Kendinizi öldürdünüz.");
+
+            }
 
         }
 
+
     }
+
+
 
 }
